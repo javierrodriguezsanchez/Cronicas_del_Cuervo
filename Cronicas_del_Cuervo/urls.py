c2a0e22ba9d6f0f1671723aefd0153042ac5c5e5
javierrodriguezsanchez <105332166+javierrodriguezsanchez@users.noreply.github.com>
2025-08-11 16:52:31 -0400

    [FIX] mas correcciones de configuracion

--- Cronicas_del_Cuervo/urls.py
+++ Cronicas_del_Cuervo/urls.py
@@ -4,8 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     # 1) Your app’s URLs go first—no conflicting `admin/` path here:
     path('', include('main.urls')),
-
-    # 2) Then the real Django admin at a different prefix
-    path('admin/', admin.site.urls),
-    
 ]
--- main/admin.py
+++ main/admin.py
@@ -1,11 +1,1 @@
 from django.contrib import admin
-from .models import Noticia, Etiquetas, Categoría, Autor
-
-@admin.register(Noticia)
-class NewsItemAdmin(admin.ModelAdmin):
-    list_display = ['título', 'fecha_de_publicación', 'categoría']
-    list_filter = ['fecha_de_publicación', 'categoría']
-
-admin.site.register(Etiquetas)
-admin.site.register(Categoría)
-admin.site.register(Autor)
